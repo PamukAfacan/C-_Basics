33000c5cbe29c20467b38f32ef04170f5f67114a
pamusama <ce.fatihyilmaz@gmail.com>
2023-03-26 16:15:56 +0300

    salla gelsin abi

--- C_Lesson_2.cpp
+++ C_Lesson_2.cpp
@@ -3,16 +3,85 @@
 
 
 using namespace std;
+/*
+class Car {        // The class
+public:          // Access specifier
+    string brand;  // Attribute
+    string model;  // Attribute
+    int year;      // Attribute
+    Car(string x, string y, int z); // Constructor declaration
+};
 
-
+// Constructor definition outside the class
+Car::Car(string x, string y, int z) {
+    brand = x;
+    model = y;
+    year = z;
+}
 
 int main() {
+    // Create Car objects and call the constructor with different values
+    Car carObj1("BMW", "X5", 1999);
+    Car carObj2("Ford", "Mustang", 1969);
 
-
-
-
-
-
-
-
+    // Print values
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+    return 0;
 }
+
+*/
+
+
+/*
+class Vehicle {
+public:
+    string brand = "Ford";
+    void honk() {
+        cout << "Tuut, tuut! \n";
+    }
+};
+
+// Derived class
+class Car : public Vehicle {
+public:
+    string model = "Mustang";
+};
+
+int main() {
+    Car myCar;
+    myCar.honk();
+    cout << myCar.brand + " " + myCar.model;
+    return 0;
+}
+
+
+*/
+
+
+class MyClass {
+public:
+    void myFunction() {
+        cout << "Some content in parent class.";
+    }
+};
+
+// Another base class
+class MyOtherClass {
+public:
+    void myOtherFunction() {
+        cout << "Some content in another class.";
+    }
+};
+
+// Derived class
+class MyChildClass : public MyClass, public MyOtherClass {
+};
+
+int main() {
+    MyChildClass myObj;
+    myObj.myFunction();
+    myObj.myOtherFunction();
+    return 0;
+}
+
